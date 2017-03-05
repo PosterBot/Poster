08afbdd33dc17ec50b32160eb4dfb25dbe9a4062
A. Cherepanov <ftp27host@gmail.com>
2017-03-05 16:28:44 +0300

    Remove unused files

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-var fileManager = require('./fileManager'),
-	scheduler,
+var fileManager = require('./fileManager')
 	winston = require('winston'),
 	colors = require('colors'),
 	provider = require('./firebaseProvider');
+
 winston.cli();
 
-var publicsFile = './settings/vkpublic.json',
-	contentGrabberFile = './settings/contentGrabber.json',
-	channelsFile = './settings/telegramchannel.json',
-	publicsList = fileManager.readDataFromJson(publicsFile),
-	//grabberSettings, = fileManager.readDataFromJson(contentGrabberFile),
-	channelsList = fileManager.readDataFromJson(channelsFile),
-	scheduler;
-
 	provider.init(function() {
-		scheduler = require('./scheduler')();
+		var scheduler = require('./scheduler')();
 		provider.getChannels(provider.API.telegram, function(list) {
 			scheduler.setTelegramPostTimer(list)
 		});
 	});
-
-	/*if(settings && publicsList){
-		scheduler = require('./scheduler')(settings);
-		scheduler.setPostTimer(publicsList);
-	} else {
-		winston.log('error', 'error file reading -> exit')
-	}*/
-
-	/*if(settings && channelsList){
-		if(!scheduler){
-			scheduler = require('./scheduler')(settings);
-		}
-		scheduler.setTelegramPostTimer(channelsList)
-
-	}*/
-
-	/*if(grabberSettings){
-		for(prop in grabberSettings){
-			scheduler.setContentGrabberTimer(grabberSettings[prop]);
-		}
-	}*/
 
 	function log(level, message) {
 	  // TODO: Need to create a global method with a enum of message groups
