464dbdc1e7b5553b3aca991baa46218f28b64cbb
jemsgit <jemguns@gmail.com>
2017-03-21 00:03:47 +0400

    add styles

--- src/app/projects/projects.controller.js
+++ src/app/projects/projects.controller.js
@@ -1,95 +1,109 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('angularPoster')
-    .controller('ProjectsController', ['firebaseService','$firebaseObject', function(firebaseService, $firebaseObject){
-        var vm = this,
-            firebaseSettings,
-            baseObject,
-            data;
-        vm.projects = {
-                vk:[],
+    angular
+        .module('angularPoster')
+        .controller('ProjectsController', ['firebaseService', '$firebaseObject', function (firebaseService, $firebaseObject) {
+            var vm = this,
+                firebaseSettings,
+                baseObject,
+                data;
+            vm.projects = {
+                vk: [],
                 telegram: []
             };
-        vm.postModel = {
+            vm.postModel = {
                 text: '',
-                link:''
+                link: ''
             }
-        vm.content = ''
+            vm.content = ''
+            vm.activeTab = 'edit';
 
-        
-        
+            function init() {
+                firebaseService.init().then(function (reference) {
+                    baseObject = $firebaseObject(reference);
+                    baseObject.$loaded()
+                        .then(function () {
+                            console.log(baseObject);
+                            if (baseObject.settings && baseObject.settings.channels) {
+                                if (baseObject.settings.channels.telegram) {
+                                    $.map(baseObject.settings.channels.telegram, function (value, key) {
+                                        var item = { name: key, params: value }
+                                        if (baseObject.content && baseObject.content.telegram && baseObject.content.telegram[key]) {
+                                            var content = []
+                                            $.map(baseObject.content.telegram[key], function (baseObject) {
+                                                var dataList = baseObject.split(' ');
+                                                var result = {
+                                                    link: dataList.splice(0, 1)[0],
+                                                    message: dataList.join(' ')
+                                                };
 
-        function init(){
-            firebaseService.init().then(function(reference){
-               baseObject = $firebaseObject(reference);
-               baseObject.$loaded()
-                .then(function() {
-                    console.log(baseObject);
-                    if(baseObject.settings && baseObject.settings.channels){
-                        if(baseObject.settings.channels.telegram){
-                            $.map(baseObject.settings.channels.telegram, function(value, key){
-                                var item = {name: key, params: value}
-                                if(baseObject.content && baseObject.content.telegram && baseObject.content.telegram[key]){
-                                    var content = []
-                                    $.map(baseObject.content.telegram[key], function(baseObject){
-                                        content.push(baseObject);
-                                    });
-                                    item.content = content
+                                                content.push(result);
+                                            });
+                                            item.content = content
+                                        }
+                                        vm.projects.telegram.push(item)
+                                    })
                                 }
-                                vm.projects.telegram.push(item)
-                         })
-                        }
-                        if(baseObject.settings.channels.vk){
-                            $.map(baseObject.settings.channels.vk, function(value, key){
-                                var item = {name: key, params: value}
-                                if(baseObject.content && baseObject.content.vk && baseObject.content.vk[key]){
-                                    var content = []
-                                    $.map(baseObject.content.vk[key], function(baseObject){
-                                        content.push(baseObject);
-                                    });
-                                    item.content = content
-                                }
-                                vm.projects.vk.push(item)
-                         })
-                        }
-                    
-                        vm.setActiveProject(vm.projects.telegram[0]);
+                                if (baseObject.settings.channels.vk) {
+                                    $.map(baseObject.settings.channels.vk, function (value, key) {
+                                        var item = { name: key, params: value }
+                                        if (baseObject.content && baseObject.content.vk && baseObject.content.vk[key]) {
+                                            var content = []
+                                            $.map(baseObject.content.vk[key], function (baseObject) {
+                                                var dataList = baseObject.split(' ');
+                                                var result = {
+                                                    link: dataList.splice(0, 1)[0],
+                                                    message: dataList.join(' ')
+                                                };
 
-                         
-                    }
+                                                content.push(result);
+                                            });
+                                            item.content = content
+                                        }
+                                        vm.projects.vk.push(item)
+                                    })
+                                }
+
+                                vm.setActiveProject(vm.projects.telegram[0]);
+
+
+                            }
+                        })
+                        .catch(function (err) {
+                            console.error(err);
+                        });
                 })
-                .catch(function(err) {
-                    console.error(err);
-                });
-            })
-        }
-
-        init();
-
-        vm.setActiveProject = function(project){
-            if(project){
-                vm.currentProject = project;
-            }   
-        }
-
-        vm.addPost = function(){
-            if(!vm.currentProject.content){
-                vm.currentProject.content = [];
             }
-            baseObject.content.telegram.testChannelJem[123] = vm.postModel.text + ' ' + vm.postModel.link;
-            baseObject.$save().then(function(data){
-                vm.currentProject.content.push(vm.postModel.text + ' ' + vm.postModel.link);
-                vm.postModel.text ='';
-                vm.postModel.link = ''
-            })
-        }
 
-        vm.showContent = function($fileContent){
-            vm.content = $fileContent;
-        };
+            init();
 
-    }]);
+            vm.setActiveProject = function (project) {
+                if (project) {
+                    vm.currentProject = project;
+                }
+            }
+
+            vm.addPost = function () {
+                if (!vm.currentProject.content) {
+                    vm.currentProject.content = [];
+                }
+                baseObject.content.telegram.testChannelJem[123] = vm.postModel.text + ' ' + vm.postModel.link;
+                baseObject.$save().then(function (data) {
+                    vm.currentProject.content.push(vm.postModel.text + ' ' + vm.postModel.link);
+                    vm.postModel.text = '';
+                    vm.postModel.link = ''
+                })
+            }
+
+            vm.saveImportData = function () {
+
+            }
+
+            vm.showContent = function ($fileContent) {
+                vm.content = $fileContent;
+            };
+
+        }]);
 
 })();
